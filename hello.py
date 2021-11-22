6dd3dc2f50ec99b2acbb25944825037ececc70d0
imamsaheb <sameer.shaikh14@gmail.com>
2021-11-22 21:47:16 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,9 @@ from json2html import *
 r = requests.get("https://reqres.in/api/users?page=2")
 response_val = dict(r.json())
 val = response_val.get('data')
-#print(val)
+for ele in val:
+    avatar = ele['avatar']
+    ele['avatar'] = '<img src="' + avatar + '"' + '/>'
 html_data = json2html.convert(json=val)
 print(html_data)
 file1 = open("index.html", "w")
