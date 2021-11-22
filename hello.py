49018a4e1fe2b1210bb83aa303936ad33a5a2007
imamsaheb <sameer.shaikh14@gmail.com>
2021-11-22 21:08:05 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello-world")
+import requests
+from json2html import *
+r = requests.get("https://reqres.in/api/users?page=2")
+response_val = dict(r.json())
+val = response_val.get('data')
+#print(val)
+html_data = json2html.convert(json=val)
+print(html_data)
+file1 = open("repor.html", "w")
+file1.write(html_data)
