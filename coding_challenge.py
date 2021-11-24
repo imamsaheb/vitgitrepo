0440fc1c6d593dc8ebd30f388dbf222f01e0c68c
imamsaheb <sameer.shaikh14@gmail.com>
2021-11-24 17:34:12 +0530

    Update coding_challenge.py

--- coding_challenge.py
+++ coding_challenge.py
@@ -17,17 +17,20 @@ class challenge():
         r = requests.get("https://reqres.in/api/users?page=2")
         response_val = dict(r.json())
         val = response_val.get('data')
+        print("json data", val)
         open_tag = "<"
         closed_tag = ">"
         val1 = json2html.convert(json=val)
         for ele in val:
             avatar = ele['avatar']
             ele['avatar'] = open_tag + 'img src="' + avatar + '"' + closed_tag
-        print(val)
+        print("after adding img src", val)
         html_data = json2html.convert(json=val)
+        print("json to html", html_data)
         data1 = html_data.replace("&lt;", "<")
         data2 = data1.replace("&gt;", ">")
         data3 = data2.replace("&quot;", '"')
+        print("after replacing special character", data3)
         file1 = open("users.html", "w")
         file1.write(data3)
         pdfkit.from_string(val1, 'users_pdf.pdf')
