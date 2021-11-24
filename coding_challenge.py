93eae5d1f7933acec9949f4f7c6cc69e94cc7574
imamsaheb <sameer.shaikh14@gmail.com>
2021-11-24 14:19:39 +0530

    Create coding_challenge.py

---
+++ coding_challenge.py
@@ -0,0 +1,22 @@
+import requests
+from json2html import *
+import pdfkit
+import os
+
+r = requests.get("https://reqres.in/api/users?page=2")
+response_val = dict(r.json())
+val = response_val.get('data')
+open_tag = "<"
+closed_tag = ">"
+for ele in val:
+    avatar = ele['avatar']
+    ele['avatar'] = open_tag + 'img src="' + avatar + '"' + closed_tag
+print(val)
+html_data = json2html.convert(json=val)
+data1 = html_data.replace("&lt;", "<")
+data2 = data1.replace("&gt;", ">")
+data3 = data2.replace("&quot;", '"')
+file1 = open("index.html", "w")
+file1.write(data3)
+#pdfkit.from_file('index.html', 'testpdf.pdf')
+os.system('wkhtmltopdf index.html testpdf.pdf')
