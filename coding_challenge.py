fc4ed20cd3f3411b88184b4b4f10b605f8c6cb60
imamsaheb <sameer.shaikh14@gmail.com>
2021-11-24 17:25:16 +0530

    Update coding_challenge.py

--- coding_challenge.py
+++ coding_challenge.py
@@ -29,7 +29,7 @@ class challenge():
         data3 = data2.replace("&quot;", '"')
         file1 = open("users.html", "w")
         file1.write(data3)
-        pdfkit.from_string(html_data, 'users_pdf.pdf')
+        pdfkit.from_string(val, 'users_pdf.pdf')
 
 obj = challenge()
 obj.fetch_users_generate_report()
